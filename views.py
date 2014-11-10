24426b989a7999cdbc1c605c286577d7699d1ded
Gaurav <goyal1092.gmailcom>
2014-11-10 16:57:24 +0530

    Fixed style isuue and about page, added 404 page

--- views.py
+++ views.py
@@ -5,6 +5,14 @@ from app import app
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/404.html')
+def static_404():
+    return render_template('404.html')
