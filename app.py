86c8e8ff80a0dff0cd3c8e7972ab2152b7c53676
Gaurav <goyal1092.gmailcom>
2014-11-11 14:35:08 +0530

    Added blog with help of flatpages

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template_string
+from flask_flatpages import FlatPages, pygmented_markdown
 
 app = Flask(__name__)
+blogs = FlatPages(app)
+
+def prerender_jinja(text):
+    return pygmented_markdown(render_template_string(Markup(text)))
+
+app.config['FLATPAGES_ROOT'] = 'blog'
+app.config['FLATPAGES_EXTENSION'] = '.md'
+app.config['FLATPAGES_HTML_RENDERER'] = prerender_jinja
 
 app.config['FREEZER_DESTINATION'] = 'gh-pages'
 app.config['FREEZER_DESTINATION_IGNORE'] = [
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app import blogs
 
 @app.route('/')
 def index():
@@ -8,6 +9,25 @@ def index():
 @app.route('/about.html')
 def about():
     return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+def page_list(pages, limit=None):
+    p_list = (p for p in pages if 'published' in p.meta)
+    p_list = sorted(p_list, reverse=True, key=lambda p: p.meta['published'])
+    return p_list[:limit]
+
+@app.route('/blog.html')
+def blog_index():
+    blog_list = page_list(blogs)
+    return render_template('blog-index.html', blog_list=blog_list)
+
+@app.route('/blog/<path:path>.html')
+def blog_detail(path):
+    blog = blogs.get_or_404(path)
+    return render_template('blog-detail.html', blog=blog)
 
 @app.errorhandler(404)
 def page_not_found(e):
